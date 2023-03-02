6b89d1f4550c684a37e45d2005ca2a29460fb227
Facundo Perez Schneider <facup200718@gmail.com>
2023-03-02 01:31:15 -0300

    Clase 6 y 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //Los strings son inmutables
+        //Los strings son inmutables.
         char[] stringArray = {'s', 't', 'r', 'i', 'n', 'g'};
         var string = new String(stringArray);
         System.out.println(string);
@@ -8,16 +8,16 @@ public class Main {
         String myString = "Hello World";
         System.out.println(myString.length());
         System.out.println(myString.charAt(4));
-        //El método concat solo nos permite concatenar strings
+        //El método concat solo nos permite concatenar strings.
         System.out.println(myString.concat("!"));
-        //En cambio, usando el operador "+" podemos concatenar distintos tipos de objetos y variables
-        //Cada vez que agregamos un "+", estamos guardando un string nuevo en el espacio de la memoria
+        //En cambio, usando el operador "+" podemos concatenar distintos tipos de objetos y variables.
+        //Cada vez que agregamos un "+", estamos guardando un string nuevo en el espacio de la memoria.
         System.out.println(myString + "!");
 
         var mySecondString = new String("Goodbye");
         System.out.println(mySecondString);
 
-        //Los StringBuffers son mutables y las operaciones son en orden siempre ya que no se crean nuevos threads
+        //Los StringBuffers son mutables y las operaciones son en orden siempre ya que no se crean nuevos threads.
         var stringBuffer = new StringBuffer("Hello World");
         System.out.println(stringBuffer);
         stringBuffer.append("!");
@@ -30,5 +30,16 @@ public class Main {
         System.out.println(stringBuffer);
         stringBuffer.reverse();
         System.out.println(stringBuffer);
+
+        var stringBuffer2 = new StringBuffer("Hello World");
+        stringBuffer2.append("!").insert(5, "oo").replace(0, 7, "Hey there,").delete(9, 16).reverse();
+        System.out.println(stringBuffer2);
+
+        //Los StringBuilders son como los StringBuffers pero mas eficientes.
+        //El tema es que varios threads pueden acceder a sus operaciones en simultáneo, no garantizando que se ejecuten en orden.
+        //O sea, la clase StringBuilder es la clase String pero mutable.
+        var stringBuilder = new StringBuilder("Hello world");
+        System.out.println(stringBuilder);
+        //También tenemos los métodos append, insert, replace, y delete.
     }
 }
